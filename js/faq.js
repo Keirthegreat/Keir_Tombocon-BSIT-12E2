895afc3b333aa1c1cb39e4801f99b0129f7aaf17
KeirSteph <155670530+Keirthegreat@users.noreply.github.com>
2025-05-17 20:45:58 +0800

    Update faq.js

--- js/faq.js
+++ js/faq.js
@@ -4,7 +4,12 @@ document.getElementById('surprise-btn').addEventListener('click', function() {
     rickroll.style.display = 'block';
     const iframe = rickroll.querySelector('iframe');
     if (iframe) {
+
+      iframe.setAttribute('allow', 'autoplay');
+      iframe.setAttribute('allowfullscreen', '');
+
       iframe.src = 'https://www.youtube.com/embed/GBIIQ0kP15E?autoplay=1';
+  
       if (iframe.requestFullscreen) {
         iframe.requestFullscreen();
       } else if (iframe.webkitRequestFullscreen) {
